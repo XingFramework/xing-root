7c7a871cee3a42939e208585e5a95bbb783d7b03
patriciaho <patricia.p.ho@gmail.com>
2015-10-17 15:34:04 -0700

    backend.rake converted with spec

---
+++ lib/xing/tasks/backend.rb
@@ -0,0 +1,45 @@
+require 'xing/edicts'
+require 'xing/tasks/tasklib'
+
+module Xing
+  module Tasks
+    class Backend < Tasklib
+      default_namespace :backend
+      setting :dir, "backend"
+
+      def define
+        in_namespace do
+          edict_task :bundle_install, Edicts::CleanRun do |bi|
+            bi.shell_cmd = %w{bundle check || bundle install}
+          end
+
+          edict_task :check_dependencies, Edicts::CleanRun do |cd|
+            cd.shell_cmd = %w{bundle exec rake dependencies:check}
+          end
+          task :check_dependencies => :bundle_install
+
+          desc "Migrate database up to current"
+          edict_task :db_migrate, Edicts::CleanRun do |dm|
+            dm.shell_cmd = %w{bundle exec rake db:migrate}
+          end
+          task :db_migrate => :bundle_install
+
+          task :setup => [:bundle_install, :db_migrate]
+
+          edict_task :db_seed, Edicts::CleanRun do |ds|
+            ds.shell_cmd = %w{bundle exec rake db:seed}
+          end
+          task :db_seed => :db_migrate
+
+          desc "Precompile rails assets"
+          edict_task :assets_precompile, Edicts::CleanRun do |ap|
+            ap.shell_cmd = %w{bundle exec rake assets:precompile}
+          end
+          task :assets_precompile => [:bundle_install, :db_migrate]
+
+          task :all => [:db_seed, :assets_precompile]
+        end
+      end
+    end
+  end
+end
---
+++ spec/tasks/backend_spec.rb
@@ -0,0 +1,25 @@
+require 'xing/tasks/backend'
+
+
+describe Xing::Tasks::Backend do
+  before :each do
+    Rake.application = nil
+    Xing::Tasks::Backend.new do |be|
+      be.dir = "test-dir"
+    end
+  end
+
+  it "creates all the backend rake tasks" do
+    expect(Rake.application.lookup "backend:bundle_install").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:check_dependencies").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:db_migrate").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:setup").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:db_seed").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:assets_precompile").to be_a(Rake::Task)
+    expect(Rake.application.lookup "backend:all").to be_a(Rake::Task)
+  end
+
+  after :each do
+    Rake.application = nil
+  end
+end
